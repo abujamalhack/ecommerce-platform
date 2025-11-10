1bc21e1c4042eb8e07fe50168702fbd43a1c1e54
𝐴𝑏𝑢 𝐽𝑎𝑚𝑎𝑙 | CyberPhantom <mccarthy29mp@gmail.com>
2025-11-10 19:39:58 +0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import Navbar from './components/Layout/Navbar'
+import Footer from './components/Layout/Footer'
+import Home from './pages/Home'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Products from './pages/Products'
+import Dashboard from './pages/user/Dashboard'
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50 flex flex-col">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
+  )
+}
+
+export default App
